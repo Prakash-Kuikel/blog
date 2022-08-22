60ecb9b4dd817cc5c7730751398b5fb968133645
Prakash <prakash.kuikel@selise.ch>
2022-08-22 16:29:55 +0600

    Setup Graphql

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.3', '>= 7.0.3.1'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'graphql'
 
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
---
+++ spec/support/graphql_helper.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+def execute(query, current_user: nil, variables: nil)
+  HashWithIndifferentAccess.new(
+    MeetingRoomBookingBeSchema.execute(query,
+                                       context: { current_user: current_user }, variables: variables).as_json
+  )
+end
+
+# For Queries
+# When you pass a key, your expectation should read as
+#   :=> user, errors = formatted_response.... where key: user
+# Without the key it should read as
+#   :=> data, errors = formatted_response...
+#
+# For Mutations
+# You will mostly be passing the key, so your expectation can look like
+#   :=> response, errors = formatted_response...
+# where response will either be a boolean flag or an object based on what is returned
+def formatted_response(query, current_user: nil, key: nil, variables: nil)
+  response = execute(query, current_user: current_user, variables: variables)
+  data = response[:data]
+  [RecursiveOpenStruct.new(key ? data[key] : data), response[:errors]]
+rescue StandardError
+  [response[:data], response.dig(:errors, 0, :message)] # for easier debugging during failures
+end
+
+def paginated_collection(node, query_string, current_user: nil)
+  response = execute(query_string, current_user: current_user)
+  [
+    response.dig(:data, node, :edges)&.pluck(:node),
+    response[:errors]
+  ]
+rescue StandardError
+  error = response.dig(:errors, 0)
+
+  ap case error.class
+     when Hash
+       error[:message]
+     else
+       error
+     end
+end
+
+# def graphql_response(klass, user, params = {})
+#   resolver = klass.new(field: nil, object: nil, context: { current_user: user })
+#   query(resolver, **params)
+# rescue StandardError => e
+#   record = e.try(:record)
+#   errors = record.try(:errors).present? ? record.errors.full_messages.to_sentence : e.message
+#   [nil, errors]
+# end
+#
+# def query(resolver, **params)
+#   response = if resolver.class.include?(SearchObject::Base)
+#                params.blank? ? resolver.resolve_with_support : resolver.resolve_with_support(**params)
+#              else
+#                params.blank? ? resolver.resolve : resolver.resolve(**params)
+#              end
+#   [response, nil]
+# end
+
+# Formats the given input hash to a GraphQL string.
+# Pass the default required key values for a given spec file in a block to avoid
+# passing it in every query param. eg.
+#   super { { projectId: project.id } }
+#
+# @param [Hash] params Keyword arguments
+# @param [String]
+def query_string(params)
+  params.merge!(yield) if block_given?
+  return if params.blank?
+
+  array = params.reduce([]) do |arr, param|
+    key, value = param
+    formatted = value.is_a?(String) ? "\"#{value}\"" : value
+    arr << "#{key.to_s.camelize(:lower)}:#{formatted}"
+  end
+
+  array.try { |item| "(#{item.join(',')})" }
+end
+
+def connection_query(request, response, meta: nil)
+  <<~GQL
+    query {
+      #{request} {
+        #{meta}
+        edges {
+          node {
+            #{response}
+          }
+        }
+        pageInfo {
+          endCursor
+          startCursor
+          hasNextPage
+          hasPreviousPage
+        }
+      }
+    }
+  GQL
+end
