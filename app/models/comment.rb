7e4ddc21a6486b2f60e1e646871917a3ffa47ed3
Prakash <prakash.kuikel@selise.ch>
2022-08-23 12:27:09 +0600

    Github CI

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  validates :body, presence: true
-
   belongs_to :user
   belongs_to :post
+
+  validates :body, presence: true
+  validates_associated :user
+  validates_associated :post
 end
