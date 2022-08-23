e6892230afa6baf781675d6e13e4bc3c691859fd
Prakash <prakash.kuikel@selise.ch>
2022-08-23 10:23:34 +0600

    Add assigner class

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
 
 class ApplicationService
-  attr_accessor :args
+  include Assigner
 
   def initialize(args = {})
-    @args = args.to_h
+    assign_attributes(args)
   end
 
-  def self.call(args = {})
+  def self.call(args)
     new(args).call
   end
+
+  def call
+    raise NotImplementedError
+  end
 end
+
+#        Example service
+#
+# class Creator < ApplicationService
+#   attr_accessor :title, :body
+#   def call
+#     Post.create(title: title, body: body)
+#   end
+# end
+#
+# Creator.call(title: 'Lorem', body: 'landvlk')
