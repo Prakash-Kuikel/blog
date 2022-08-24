caae0b49a8d69855768a5c7c5fa3058d5c202347
Prakash <prakash.kuikel@selise.ch>
2022-08-24 16:04:30 +0600

    Add updateUser mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,10 @@
 
 module Types
   class MutationType < Types::BaseObject
+    include MountMutation
+
     mount_mutation Mutations::Users::Create
     mount_mutation Mutations::Users::Delete
+    mount_mutation Mutations::Users::Update
   end
 end
